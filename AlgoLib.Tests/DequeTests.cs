52a6460f8d0caeb95da3e808ee4893ac18becac6
dboikliev <deyanpb@hotmail.com>
2017-11-12 23:00:05 +0200

    Deque data structure implementation

---
+++ AlgoLib.Tests/DequeTests.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgoLib.Queues;
+using Xunit;
+
+namespace AlgoLib.Tests
+{
+    public class DequeTests
+    {
+        [Fact]
+        public void Count_ShouldBe0_ForEmptyQueue()
+        {
+            var deque = new Deque<int>();
+            
+            Assert.Equal(0, deque.Count);
+        }
+        
+        [Fact]
+        public void DequeueFirst_ShouldThrowException_ForEmptyQueue()
+        {
+            var deque = new Deque<int>();
+
+            var ex = Assert.Throws<InvalidOperationException>(() => deque.DequeueFirst());
+            
+            Assert.Equal("The queue is empty.", ex.Message);
+        }
+        
+        [Fact]
+        public void DequeueLast_ShouldThrowException_ForEmptyQueue()
+        {
+            var deque = new Deque<int>();
+
+            var ex = Assert.Throws<InvalidOperationException>(() => deque.DequeueLast());
+            
+            Assert.Equal("The queue is empty.", ex.Message);
+        }
+        
+        [Fact]
+        public void DequeueFirstAfterEnqueueLast_ShouldReturnElementsInOrderOfEnqueueing()
+        {
+            var deque = new Deque<int>();
+
+            var numbers = Enumerable.Range(1, 60).ToArray();
+            foreach (var x in numbers)
+            {
+                deque.EnqueueLast(x);
+            }
+
+            var elements = new List<int>();
+
+            while (deque.Count > 0)
+            {
+                elements.Add(deque.DequeueFirst());
+            }
+            
+            Assert.Equal(numbers, elements);
+        }
+        
+        [Fact]
+        public void DequeueLastAfterEnqueueLast_ShouldReturnElementsInReverseOrderOfEnqueueing()
+        {
+            var deque = new Deque<int>();
+
+            var numbers = Enumerable.Range(1, 60).ToArray();
+            foreach (var x in numbers)
+            {
+                deque.EnqueueLast(x);
+            }
+
+            var elements = new List<int>();
+
+            while (deque.Count > 0)
+            {
+                elements.Add(deque.DequeueLast());
+            }
+            
+            Assert.Equal(numbers.Reverse(), elements);
+        }
+        
+        [Fact]
+        public void DequeueFirstAfterEnqueueFirst_ShouldReturnElementsInReverseOrderOfEnqueueing()
+        {
+            var deque = new Deque<int>();
+
+            var numbers = Enumerable.Range(1, 60);
+            foreach (var x in numbers)
+            {
+                deque.EnqueueFirst(x);
+            }
+
+            var elements = new List<int>();
+
+            while (deque.Count > 0)
+            {
+                elements.Add(deque.DequeueFirst());
+            }
+            
+            Assert.Equal(numbers.Reverse(), elements);
+        } 
+        
+        [Fact]
+        public void DequeueLastAfterEnqueueFirst_ShouldReturnElementsInOrderOfEnqueueing()
+        {
+            var deque = new Deque<int>();
+
+            var numbers = Enumerable.Range(1, 60);
+            foreach (var x in numbers)
+            {
+                deque.EnqueueFirst(x);
+            }
+
+            var elements = new List<int>();
+
+            while (deque.Count > 0)
+            {
+                elements.Add(deque.DequeueLast());
+            }
+            
+            Assert.Equal(numbers, elements);
+        }
+        
+        [Fact]
+        public void Count_ShouldBeSameAsEnqueuedNumberOfElementsAfterEnqueueFirst()
+        {
+            var deque = new Deque<int>();
+
+            var numbers = Enumerable.Range(1, 60);
+            foreach (var x in numbers)
+            {
+                deque.EnqueueFirst(x);
+            }
+
+            Assert.Equal(numbers.Count(), deque.Count);
+        }
+        
+        [Fact]
+        public void Count_ShouldBeSameAsEnqueuedNumberOfElementsAfterEnqueueLast()
+        {
+            var deque = new Deque<int>();
+
+            var numbers = Enumerable.Range(1, 60);
+            foreach (var x in numbers)
+            {
+                deque.EnqueueLast(x);
+            }
+
+            Assert.Equal(numbers.Count(), deque.Count);
+        }
+
+    }
+}
---
+++ AlgoLib/Queues/Deque.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace AlgoLib.Queues
+{
+    public class Deque<T>
+    {
+        private const int InitialCapacity = 16;
+        
+        //[0, 0, 3, 4, 5, 0, 0, 0];
+        private T[] _elements = new T[InitialCapacity];
+        private int _head = 0;
+        private int _tail = 0;
+        private int _count = 0;
+
+        public int Count => _count; 
+        
+        public Deque<T> EnqueueFirst(T value)
+        {
+            if (_count + 1>= _elements.Length)
+                Resize();
+            
+            if (_count > 0)
+            {
+                _head--;
+                if (_head < 0)
+                {
+                    _head = _elements.Length - 1;
+                }
+            }
+                
+            _elements[_head] = value;
+            _count++;
+
+            return this;
+        }
+
+        public T DequeueFirst()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty.");
+            }
+            var result = _elements[_head];
+            
+            if (_count > 0)
+                _head = (_head + 1) % _elements.Length;
+            
+            _count--;
+            return result;
+        }
+        
+        public Deque<T> EnqueueLast(T value)
+        {
+            
+            if (_count + 1 >= _elements.Length)
+                Resize();
+
+            if (_count > 0)
+                _tail = (_tail + 1) % _elements.Length;
+            
+            _elements[_tail] = value;
+            _count++;
+
+            return this;
+        }
+
+        public T DequeueLast()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty.");
+            }
+
+            var result = _elements[_tail];
+
+            _count--;
+            _tail--;
+            if (_tail < 0)
+            {
+                _tail = _elements.Length - 1;
+            }
+
+            return result;
+        }
+
+        private void Resize()
+        {
+            var resized = new T[_elements.Length * 2];
+
+            int added = 0;
+            for (int i = _head; i < _elements.Length; i++)
+            {
+                resized[i - _head] = _elements[i];
+                added++;
+            }
+            
+            for (int i = 0; i < _head; i++)
+            {
+                resized[i + added] = _elements[i];
+            }
+
+            _elements = resized;
+
+            _head = 0;
+            _tail = _count - 1;
+        }
+    }
+}
