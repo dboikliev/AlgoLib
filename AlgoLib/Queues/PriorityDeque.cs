0c967987c51d85c0a9ff8c0d2b6ff78c6eeb8121
dboikliev <deyanpb@hotmail.com>
2017-11-16 19:41:23 +0200

    Double ended priority queue

---
+++ AlgoLib/Queues/PriorityDeque.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AlgoLib.Queues
+{
+    public class PriorityDeque<T>
+    {
+        struct PriorityDequeNode<T>
+        {
+            public T Min;
+            public T Max;
+
+            public PriorityDequeNode(T minMax) => Min = Max = minMax;
+
+            public PriorityDequeNode(T min, T max)
+            {
+                Min = min;
+                Max = max;
+            }
+        }
+        
+        private const int InitialCapacity = 16;
+        
+        private PriorityDequeNode<T>[] _elements;
+        
+        public int Count { get; private set; };
+
+        public PriorityDeque(int capacity = InitialCapacity)
+            => _elements = new PriorityDequeNode<T>[capacity];
+
+        public T DequeMin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T DequeMax()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PriorityDeque<T> Enqueue(T value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
